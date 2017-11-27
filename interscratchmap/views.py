8f818d611a4ee2a77550cf56827cefc225c05a2d
bartdm <dmitriy.bartoshchuk@gmail.com>
2017-11-28 00:19:27 +0100

    basic_signup

--- interscratchmap/views.py
+++ interscratchmap/views.py
@@ -1,5 +1,29 @@
-from django.shortcuts import render
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 
 
+# to uncomment annotation when signup will done
+# @login_required
 def index(request):
     return render(request, 'index.html')
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('index')
+    else:
+        form = UserCreationForm()
+    return render(request, 'authorization/signup.html', {'form': form})
+
+
+def login(request):
+    return render(request, 'authorization/login.html')
